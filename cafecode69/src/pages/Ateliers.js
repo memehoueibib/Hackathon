3e12cfe9509f0852ee73567004360e81a50bc076
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2024-07-03 13:39:12 +0200

    Add pictures

--- cafecode69/src/pages/Ateliers.js
+++ cafecode69/src/pages/Ateliers.js
@@ -11,17 +11,17 @@ function Ateliers() {
       <div className="content">
         <h2>Nos Ateliers</h2>
         <div className="workshops">
-          <div className="workshop">
+          <div className="workshop1">
             <h3>Initiation aux Algorithmes</h3>
             <p>Apprenez les bases des algorithmes de manière ludique. Idéal pour les enfants et les débutants en programmation.</p>
             <Link to="/atelier-algorithmes"><button>Voir Détails</button></Link>
           </div>
-          <div className="workshop">
+          <div className="workshop2">
             <h3>Tournois de Jeux Vidéo</h3>
             <p>Participez à nos compétitions de jeux vidéo. Développez vos compétences et rencontrez d'autres passionnés.</p>
             <Link to="/atelier-gaming"><button>Voir Détails</button></Link>
           </div>
-          <div className="workshop">
+          <div className="workshop3">
             <h3>Ateliers pour Seniors</h3>
             <p>Initiez-vous aux technologies modernes en toute simplicité. Parfait pour les seniors qui souhaitent se familiariser avec le numérique.</p>
             <Link to="/atelier-technologies">
